5d4d62097025958437fee0bc093ac6c7f079ae51
Pedro Marchiori <pmarchiori08@gmail.com>
2024-11-11 02:16:17 -0300

    project install

---
+++ DMRN-lista/App.js
@@ -0,0 +1,24 @@
+import { Text, SafeAreaView, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>Ola</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
