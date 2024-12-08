bd1c0d9fd5ab3a58f465fda4e852921d2d057b92
Pedro Marchiori de Araújo <pmarchiori08@gmail.com>
2024-12-08 14:46:44 -0300

    fetch api

--- DMRN-marvel_api/App.js
+++ DMRN-marvel_api/App.js
@@ -1,8 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet, Text, View } from "react-native";
 
+import CharactersListScreen from "./screens/CharactersListScreen";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="CharacterList" component={CharactersListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
--- DMRN-marvel_api/screens/CharactersListScreen.jsx
+++ DMRN-marvel_api/screens/CharactersListScreen.jsx
@@ -1,10 +1,51 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import md5 from "js-md5";
+import env from "../constants/env";
 
 export default function CharactersListScreen() {
+  const [characters, setCharacters] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [message, setMessage] = useState(false);
+
+  const fetchApi = () => {
+    setLoading(true);
+    const { apiUrl, ts, publicKey, privateKey } = env;
+
+    const hash = md5(`${ts}${privateKey}${publicKey}`);
+
+    const uri = `${apiUrl}v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+    fetch(uri)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((result) => {
+        if (result.code !== 200) {
+          throw new Error(result.status || "Erro na API");
+        }
+        setCharacters(result.data.results);
+        setMessage("Sucesso!");
+      })
+      .catch((error) => setMessage(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
     <View>
       <Text>Personagens</Text>
+      {isLoading && <ActivityIndicator />}
+      {message && <Text>{message}</Text>}
+      {characters &&
+        characters.map((char) => <Text key={char.id}>{char.name}</Text>)}
     </View>
   );
 }
